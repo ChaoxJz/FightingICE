9a11621bdfcbc4b720971d3f35df7829feee708b
Jz <86885197+ChaoxJz@users.noreply.github.com>
2022-11-18 02:45:42 +0900

    Add files via upload

--- FightingICE.bat
+++ FightingICE.bat
@@ -4,7 +4,7 @@ setlocal ENABLEDELAYEDEXPANSION
 ::-Xmx8240m -Xms8240m -Xmn5120m 
 :: close CUDA code
 set CUDA_VISIBLE_DEVICES=100
-java -cp FightingICE.jar;./lib/lwjgl/*;./lib/natives/windows/*;./lib/*;  Main  --limithp 400 400 --grey-bg --inverted-player 1 
+java -cp FightingICE.jar;./lib/lwjgl/*;./lib/natives/windows/*;./lib/*;  Main --py4j --limithp 400 400 --grey-bg --inverted-player 1 
 rem TIMEOUT /T -1
 endlocal
 pause
